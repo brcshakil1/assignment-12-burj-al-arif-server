1ad28834c90f0f41501c5429a7dce5a397eca0f3
Bayazidur Rahman Chowdhury <98935987+brcshakil1@users.noreply.github.com>
2023-11-26 02:24:23 +0600

    Add files via upload
    added file manually

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Burj Al Arif is running");
+  console.log("hello world");
+});
+
+app.listen(port, () => {
+  console.log(`Burj Al Arif is listening on ${port}`);
+});
