a3318eabb89fb2cb6209283e3c51a413b1611ed9
brcshakil1 <brcshakil72@gmail.com>
2023-11-26 09:44:18 +0600

    created a collection and get all apartments on server

--- index.js
+++ index.js
@@ -3,11 +3,51 @@ const app = express();
 
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // middlewares
 app.use(express.json());
 app.use(cors());
+
+// mongodb
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ufgx0zu.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const apartmentsCollection = client
+      .db("burjAlArifDB")
+      .collection("apartments");
+
+    // get all apartments
+    app.get("/apartments", async (req, res) => {
+      const result = await apartmentsCollection.find().toArray();
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Burj Al Arif is running");
