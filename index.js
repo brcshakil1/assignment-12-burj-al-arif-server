f66d4e9af07bd552084425f1fd96e0d06caa91f7
brcshakil1 <brcshakil72@gmail.com>
2023-11-27 21:50:22 +0600

    post agreements

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ app.use(express.json());
 app.use(
   cors({
     origin: ["http://localhost:5173"],
-    credentials: true,
-    optionSuccessStatus: 200,
   })
 );
 
@@ -42,39 +40,62 @@ async function run() {
       .db("burjAlArifDB")
       .collection("apartments");
 
+    const agreementsCollection = client
+      .db("burjAlArifDB")
+      .collection("agreements");
+
     const usersCollection = client.db("burjAlArifDB").collection("users");
 
     // auth related api
     // login
     app.post("/jwt", async (req, res) => {
       const user = req.body;
-      console.log("I need a new jwt", user);
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "365d",
       });
-      res
-        .cookie("token", token, {
-          httpOnly: true,
-          secure: process.env.NODE_ENV === "production",
-          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-        })
-        .send({ success: true });
+      res.send({ token });
     });
 
-    // logout
-    app.get("/logout", async (req, res) => {
-      try {
-        res
-          .clearCookie("token", {
-            maxAge: 0,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-          })
-          .send({ success: true });
-        console.log("logout successfully");
-      } catch (err) {
-        res.status(500).send(err);
+    // middlewares
+    const verifyToken = (req, res, next) => {
+      console.log("inside verify token", req.headers);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: "unauthorized access" });
       }
+      const token = req.headers.authorization.split(" ")[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: "unauthorized access" });
+        }
+        req.decoded = decoded;
+        next();
+      });
+    };
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const isAdmin = user?.role === "admin";
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      next();
+    };
+
+    // get admin
+    app.get("/user/admin/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (email !== email) {
+        return res.status(403).send({ message: "unauthorized access" });
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === "admin";
+      }
+      res.send({ admin });
     });
 
     // get all apartments
@@ -91,6 +112,19 @@ async function run() {
       const totalApartments =
         await apartmentsCollection.estimatedDocumentCount();
       res.send({ result, totalApartments });
+    });
+
+    // get all users
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
+    // all agreements
+    app.post("/agreements", async (req, res) => {
+      const agreementInfo = req.body;
+      const result = await agreementsCollection.insertOne(agreementInfo);
+      res.send(result);
     });
 
     // save and modify user name, email and role in DB
